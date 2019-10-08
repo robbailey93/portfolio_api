00e354aa7033cc1e1eea2a30da372bbf7b36de50
Robert Bailey <robbailey@Robs-MacBook-Air.local>
2019-10-07 21:15:34 -0600

    Adds new routes and req and res

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', {
+		title: 'My Profile API',
+		message: 'This API must be called from the REACT App',
+	});
 });
 
 module.exports = router;
---
+++ routes/profiles.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+//TODO: this is a dummy profile for API creation
+
+const dummyProfile = {
+	_id: '12',
+	firstname: 'Rob',
+	lastname: 'Bailey',
+};
+
+// TODO: Remove dummy response
+
+router.get('/', (req, res) => res.json(dummyProfile));
+
+//router.get('/:id', function(req, res, next) {
+//	res.json(dummyProfile);
+//});
+
+router.get('/:id', (req, res) => res.json(dummyProfile));
+
+// TODO: Remove dummy response
+
+router.post('/', (req, res) => res.json(dummyProfile));
+
+router.post('/', (req, res) => res.json(dummyProfile));
+
+// TODO: Need to write code to create new profile
+// TODO: Remove dummy response
+
+module.exports = router;
