26427a1ffbef32e2a34f6b6d238f3a61f2e26ac5
Robert Bailey <robbailey@Robs-MacBook-Air.local>
2019-12-03 14:20:49 -0700

    Fixes security issue in package-lock from github

--- routes/skills.js
+++ routes/skills.js
@@ -1,50 +1,85 @@
 var express = require('express');
 var router = express.Router();
-const Skills = require('../models/skill.js');
+const Skill = require('../models/skill.js');
 
+// router.get('/', function(req, res, next) {
+// 	// should return all users
+// 	Skills.find({}, function(err, data) {
+// 		/// have to provide a callback function due to it being async
+
+// 		if (err) {
+// 			return res.json({
+// 				error: 'errormsg',
+// 				message: 'There is an error in Skills',
+// 			});
+// 		}
+
+// 		res.json(data);
+// 		// look at documentation
+// 	});
+// });
+////////////
+/////
+// Define Post or Create route
+router.post('/', function(req, res) {
+	const newSkill = new Skill(req.body);
+	newSkill
+		.save()
+		.then(skill => {
+			res.status(200).json({ skill: 'new skill was added successfully' });
+			console.log(newSkill);
+		})
+		.catch(err => {
+			res.status(400).send('unable to save to database');
+		});
+});
+
+// Define Get or Read route / get all
 router.get('/', function(req, res, next) {
-	// should return all users
-	Skills.find({}, function(err, data) {
-		/// have to provide a callback function due to it being async
-
+	// should return all skills
+	Skill.find(function(err, skills) {
+		// have to provide a callback function due to it being async
 		if (err) {
-			return res.json({
-				error: 'errormsg',
-				message: 'There is an error in Skills',
-			});
+			console.log(err);
+		} else {
+			res.json(skills);
 		}
-
-		res.json(data);
-		// look at documentation
 	});
 });
 
-//TODO: this is a dummy profile for API creation
+// Define Get or Read route get by ID for the edit
+router.get('/:id', function(req, res) {
+	let id = req.params.id;
+	Skill.findById(id, function(err, skill) {
+		res.json(skill);
+	});
+});
 
-// const dummyProfile = {
-// 	_id: '12',
-// 	firstname: 'Rob',
-// 	lastname: 'Bailey',
-// 	page: 'skills',
-// };
+// Define Put or Update route get by ID
+router.put('/:id', function(req, res) {
+	Skill.findOneAndUpdate(
+		{ _id: req.params.id },
+		req.body,
+		// an option that asks mongoose to return the updated version
+		// of the document instead of the pre-updated one.
+		{ new: true },
+		(err, skill) => {
+			if (err) return res.status(500).send(err);
+			return res.status(200).send(skill);
+		},
+	);
+});
 
-// TODO: Remove dummy response
-
-router.get('/', (req, res) => res.json(dummyProfile));
-
-//router.get('/:id', function(req, res, next) {
-//	res.json(dummyProfile);
-//});
-
-router.get('/:id', (req, res) => res.json(dummyProfile));
-
-// TODO: Remove dummy response
-
-router.post('/', (req, res) => res.json(dummyProfile));
-
-router.post('/', (req, res) => res.json(dummyProfile));
-
-// TODO: Need to write code to create new profile
-// TODO: Remove dummy response
+// Define Delete route
+router.delete('/:id', function(req, res) {
+	Skill.findOneAndRemove({ _id: req.params.id }, (err, skill) => {
+		if (err) return res.status(500).send(err);
+		const response = {
+			message: 'Skill successfully deleted',
+			id: skill._id,
+		};
+		return res.status(200).send(response);
+	});
+});
 
 module.exports = router;
